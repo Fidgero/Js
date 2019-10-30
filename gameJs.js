f1f509d94fdf4f0fe680de33038bafe5471c8d3e
Fidgero <48552449+Fidgero@users.noreply.github.com>
2019-10-30 11:11:04 +0200

    Add files via upload

---
+++ gameJs.js
@@ -0,0 +1,31 @@
+var number; 
+var attempts = 0;
+
+number = Math.round(1000 + Math.random() * (9999 - 1000));
+guessNumber();
+
+function guessNumber(){
+	attempts++;
+
+	var result = parseInt(prompt("ведите число (-1 - закончить игру) " ));
+
+	if(result == number){
+		alert("Вы угадали! Число попыток: " + attempts);
+		location.reload(); 
+	}
+	else if(result == 0 || isNaN(result)){
+		alert("Вы не ввели число");
+		guessNumber();
+	}
+	else if(result == -1){
+		alert("Спасибо за игру!");
+	}
+	else{
+		if(result > number){
+			alert("Ваше число больше загаданного");
+		}
+		else
+			alert("Ваше число менье загаданного");
+		guessNumber();
+	}
+}
